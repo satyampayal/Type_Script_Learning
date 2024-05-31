0b77ec708486d9cffe78f8f4aa855ac88db1394e
satyam <satyampayal10@gmail.com>
2024-05-31 23:40:12 +0530

    TypeScript Start to Learn

--- 01Intro/intro.ts
+++ 01Intro/intro.ts
@@ -3,7 +3,22 @@
 // let user={name:"satyam",age:18};
 // console.log("satyam payal");
 // console.log(user.name);
+
 // after write code we commpile the file 
 // by ts intro.ts 
 // it generate a js file with same 
 // after this is show the error  which re-declared blocked scoped 
+/*  ---- TypeScript types  */
+
+// - Number - String - Boolean (Primitive types )
+// -Null   - Undefined -Void 
+// Object - Array -Tuple ----
+// Any -(not used by use this our code make vulnerable only some condition are used these )
+
+// --- situation ----
+// example-- A function accepts 2 Numbers
+//  example--   A function returns a string 
+
+// ------ Syntax -----------
+//let varible Name:type=value
+
--- 01Intro/varible.ts
+++ 01Intro/varible.ts
@@ -2,6 +2,8 @@
 let greetSting:string ="Satyam Payal";
 console.log(greetSting);
 
+//greetSting =4; // it give error
+ 
 // now to overcome re-declared blocked scopeed use 
 // export {} -- tempory its works
 export {}
