74325658bdfbaa48257ce417189fe74e57779546
satyam <satyampayal10@gmail.com>
2024-05-31 23:50:09 +0530

    Number and boolean

--- 01Intro/varible.ts
+++ 01Intro/varible.ts
@@ -3,7 +3,20 @@ let greetSting:string ="Satyam Payal";
 console.log(greetSting);
 
 //greetSting =4; // it give error
- 
+
+// number 
+//let uId:number=24;
+    // >> Not neccecery al time :number 
+// also you do 
+let uId=3453.67;
+uId.toFixed();
+//uId="kva";
+
+// boolean 
+let isLoggedIn:boolean =true;
+isLoggedIn=false;
+
+
 // now to overcome re-declared blocked scopeed use 
 // export {} -- tempory its works
 export {}
