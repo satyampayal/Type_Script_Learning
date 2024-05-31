1ac50dcfa71e93e2cded6c9e8d9d59c797d11c5e
satyam <satyampayal10@gmail.com>
2024-06-01 00:09:39 +0530

    Fuction use

---
+++ 01Intro/myFunction.ts
@@ -0,0 +1,17 @@
+// function addTwo(num){
+//     return num+2;
+// }
+// addTwo(5);
+/////// It is Problemtic  num is :any ..... mistake occur more 
+
+
+function addTwo(num:number){
+        return num+2;
+    }
+
+function getUpper(val:string){
+    return val.toUpperCase();
+}
+
+   getUpper("satyam");
+    addTwo(5);
---
+++ 01Intro/myfunction.js
@@ -0,0 +1,13 @@
+// function addTwo(num){
+//     return num+2;
+// }
+// addTwo(5);
+/////// It is Problemtic  num is :any ..... mistake occur more 
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+getUpper("satyam");
+addTwo(5);
