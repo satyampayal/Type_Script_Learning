009901337bb008a5d058cda24d211b900da8a175
satyam <satyampayal10@gmail.com>
2024-06-01 00:28:50 +0530

    Fuction use

--- 01Intro/myFunction.ts
+++ 01Intro/myFunction.ts
@@ -2,16 +2,54 @@
 //     return num+2;
 // }
 // addTwo(5);
-/////// It is Problemtic  num is :any ..... mistake occur more 
+/////// It is Problemtic  num is :any ..... mistake occur more
 
-
-function addTwo(num:number){
-        return num+2;
-    }
-
-function getUpper(val:string){
-    return val.toUpperCase();
+function addTwo(num: number) {
+    return num + 2;
+    // return something different so how is it handle
+    //   return "hello";
 }
 
-   getUpper("satyam");
-    addTwo(5);
+function getUpper(val: string) {
+    return val.toUpperCase();
+}
+function signUpUser(name: string, email: string, password: string) { }
+
+var loginUser = (name: string, email: string, isPaid: boolean = false) => {
+
+}
+loginUser("satyam", "sat@ga.com");
+signUpUser("satyam", "abc@gmail.com", "1234");
+
+getUpper("satyam");
+let num: number = addTwo(5);
+
+//return more tehn one  data types like
+//  function getValue(val:number):boolean{
+//     if(val>5){
+//         return true;
+//     }
+//     return  "status ok";
+//  }
+
+var getHello = (s: string): string => {
+    return "";
+
+}
+
+const heroes=["thor","spideramn","Iron MAN","CA"];
+heroes.map((hero):string =>{
+    return `hero is ${hero}`;
+   // return 2;
+})
+// map already have detail information about ts
+
+function showErrorMsg(errmsg:string):void{
+    console.log(errmsg);
+    
+}
+function handleErrorMsg(errmsg:string):never{
+    throw new Error(errmsg);
+    
+}
+
--- 01Intro/myfunction.js
+++ 01Intro/myfunction.js
@@ -2,12 +2,42 @@
 //     return num+2;
 // }
 // addTwo(5);
-/////// It is Problemtic  num is :any ..... mistake occur more 
+/////// It is Problemtic  num is :any ..... mistake occur more
 function addTwo(num) {
     return num + 2;
+    // return something different so how is it handle
+    //   return "hello";
 }
 function getUpper(val) {
     return val.toUpperCase();
 }
+function signUpUser(name, email, password) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+loginUser("satyam", "sat@ga.com");
+signUpUser("satyam", "abc@gmail.com", "1234");
 getUpper("satyam");
-addTwo(5);
+var num = addTwo(5);
+//return more tehn one  data types like
+//  function getValue(val:number):boolean{
+//     if(val>5){
+//         return true;
+//     }
+//     return  "status ok";
+//  }
+var getHello = function (s) {
+    return "";
+};
+var heroes = ["thor", "spideramn", "Iron MAN", "CA"];
+heroes.map(function (hero) {
+    return "hero is ".concat(hero);
+    // return 2;
+});
+// map already have detail information about ts
+function showErrorMsg(errmsg) {
+    console.log(errmsg);
+}
+function handleErrorMsg(errmsg) {
+    throw new Error(errmsg);
+}
