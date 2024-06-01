a67c034a9c493c8e4ad5a5f27daa27b025d1ba90
satyam <satyampayal10@gmail.com>
2024-06-01 19:43:36 +0530

    Enum

--- 02Bascic/myInterfcae.ts
+++ 02Bascic/myInterfcae.ts
@@ -8,8 +8,24 @@ interface User{
    startTrial():String
    getCuppon(couponname:string):number
 }
+
+// we are not need to reopening 
+interface User{
+    githubToken:string
+}
+// inherit  
+// you can change in interface 
+// where after :type you can't change in defination 
+interface Admin extends User{
+    role:"admin" | "learners"
+}
+// type do like this
+
+// type admin =User & 
+//{ honey:string}
 const satyam:User={dbId:12,email:"s@.com",
 userId:121,
+githubToken:"Token ",
 startTrial:()=>{
     return "trial Start"
 
