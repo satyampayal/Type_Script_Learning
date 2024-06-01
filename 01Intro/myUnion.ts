93bbf630e65200586244d5a822291bedb290e159
satyam <satyampayal10@gmail.com>
2024-06-01 08:22:05 +0530

    some array

---
+++ 01Intro/myUnion.ts
@@ -0,0 +1,41 @@
+// let score: number | string | boolean = 33
+
+// score = 33
+// score = "55"
+// score = true
+
+// type User = {
+//     name: string
+//     id: string
+// }
+
+// type Admin = {
+//     username: string
+//     id: number
+// }
+
+// let hitesh: User | Admin = { username: "satyam", id: 12 }
+
+// // function getDBid(id:number |  string){
+// //     console.log("DB id is"+id);
+// // }
+// getDBid(3);
+// getDBid("sdjhg");
+// function getDBid(id: number | string) {
+//     if (typeof (id) == "string") {
+//         id.toLowerCase();
+//     }
+//     console.log("DB id is" + id);
+
+// }
+
+// // array
+// const data: number[] = [1, 2, 3, 4];
+// const data2: string[] = ["1", "2", "3"]
+// const data3: (string | number)[] = [1, "2", 3];
+
+
+// // seatAlotment 
+// let seatAllotment:"alise"|"middle"| "window"
+// seatAllotment="window";
+// // this varible can use only 3 words
