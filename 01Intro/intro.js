cbde8549e80f2b8f22f38254be4657cc96ab0cb3
satyam <satyampayal10@gmail.com>
2024-06-01 08:01:48 +0530

    Array completed

--- 01Intro/intro.js
+++
@@ -1,3 +0,0 @@
-var user = { name: "satyam", age: 18 };
-console.log("satyam payal");
-console.log(user.name);
---
+++ 01Intro/myArray.ts
@@ -0,0 +1,20 @@
+
+const superHeroes: string[]=[]
+//const heroPower:number[]=[] also do as
+const heroPower:Array<number>=[];
+
+type User={
+    name:string
+    isActive:boolean
+}
+const alluser:User[]=[];
+
+const TwodArray:number[][]=[
+    [1,2,3],
+    [4,5,6]
+]
+
+
+superHeroes.push("spiderman")
+heroPower.push(2);
+alluser.push({name:"satyam",isActive:false})
