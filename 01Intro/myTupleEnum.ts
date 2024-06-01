6b4f7ecee2b160c7c5a99008cda38f06ac850293
satyam <satyampayal10@gmail.com>
2024-06-01 08:39:43 +0530

    Tuple

---
+++ 01Intro/myTupleEnum.ts
@@ -0,0 +1,14 @@
+
+// //const user:(string | number )[]=["hello",1];
+// // tuple start
+// const user:[number,string,boolean]=[1,"satyam",true];// order maintain 
+
+// let rgb:[number,number,number]=[255,123,112];
+
+// type User=[number,string]
+// const newuser:User=[112,"satyam"];
+
+// // tuple valuse can change
+// newuser[1]="hc.com";
+// // but why Ts allow to push pop where tuple gurante to length fix 
+// newuser.push(1);
